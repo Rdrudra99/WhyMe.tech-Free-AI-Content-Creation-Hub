62daf1a3ab93985d6dc3dbc849f251fddb67ba26
Rudra Narayan Boitei <rd.rudra99@gmail.com>
2024-07-01 19:53:14 +0530

    added og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,37 @@ export const metadata: Metadata = {
   title: "Vyākhyā AI",
   description:
     "Vyākhyā AI is a modern, fast, and lightweight content Generator tool website that helps you generate content for your website, blog, or social media.",
+    keywords: [
+      "Vyākhyā AI",
+      "Vyākhyā",
+      "AI",
+      "content generator",
+      "content",
+      "generator",
+      "tool",
+      "website",
+      "blog",
+      "social media",
+      "modern",
+    ],
+    authors: [
+      {
+        name: "Rdrudra99",
+        url: "https://rdrudra99.site",
+      },
+    ],
+    creator: "Rdrudra99",
+    openGraph: {
+      type: "website",
+      images: [
+        {
+          url: "https://i.ibb.co/m9RxLMd/20240701-194337-0000.png",
+          width: 1200,
+          height: 630,
+          alt: "Vyākhyā AI",
+        },
+      ],
+    },
 };
 
 export default function RootLayout({
