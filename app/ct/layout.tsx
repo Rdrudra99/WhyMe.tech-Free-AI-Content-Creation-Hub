692da8c48a7e744a3d7c9db1641464aa6a17093c
Rudra Narayan Boitei <rd.rudra99@gmail.com>
2024-08-14 12:35:03 +0530

    commit

--- app/ct/layout.tsx
+++ app/ct/layout.tsx
@@ -4,20 +4,18 @@ import { cn } from '@/lib/utils'
 import React from 'react'
 
 const layout = (
-    {children}: Readonly<{children: React.ReactNode;}>,
+    { children }: Readonly<{ children: React.ReactNode; }>,
 ) => {
-  return (
-    <div
-          className={cn(
-            "relative flex min-h-screen w-full bg-background font-sans text-foreground antialiased",
-          )}
+    return (
+        <div
+            className={cn(
+                "relative flex min-h-screen w-full bg-background font-sans text-foreground antialiased",
+            )}
         >
-
-            {/* <NextTopLoader color="#2299DD" showSpinner={false} speed={400} /> */}
             <DocsSidebarNav items={docsConfig.sidebarNav} />
             <main className="flex-1">{children}</main>
         </div>
-  )
+    )
 }
 
 export default layout
