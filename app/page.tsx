e1b1a8c8976a530a18bbe32b30a54060932d302f
Rudra Narayan Boitei <rd.rudra99@gmail.com>
2024-08-16 20:12:05 +0530

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -40,7 +40,7 @@ const page = () => {
                 </div>
               ) : (
                 <div className="flex space-x-2 items-center">
-                  <Link className="relative z-10 bg-transparent hover:bg-gray-100 border border-transparent text-black text-sm md:text-sm transition font-medium duration-200 rounded-full px-4 py-2 flex items-center justify-center dark:text-white dark:hover:bg-neutral-800 dark:hover:shadow-xl" href="/log-in">Login</Link>
+                  <Link className="relative z-10 bg-transparent hover:bg-gray-100 border border-transparent text-black text-sm md:text-sm transition font-medium duration-200 rounded-full px-4 py-2 flex items-center justify-center dark:text-white dark:hover:bg-neutral-800 dark:hover:shadow-xl" href="/sign-in">Login</Link>
                   <Link className="bg-neutral-900 relative z-10 hover:bg-black/90 border border-transparent text-white text-sm md:text-sm transition font-medium duration-200 rounded-full px-4 py-2 flex items-center justify-center shadow-[0px_-1px_0px_0px_#FFFFFF40_inset,_0px_1px_0px_0px_#FFFFFF40_inset]" href="/sign-up">Sign Up</Link>
                 </div>
               )
