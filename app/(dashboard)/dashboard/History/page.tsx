a6ffddf2fcbce347a8af505e3e1a9969b46e907b
Rudra Narayan Boitei <rd.rudra99@gmail.com>
2024-08-17 17:15:49 +0530

    Fix the Copy Output issue

--- app/(dashboard)/dashboard/History/page.tsx
+++ app/(dashboard)/dashboard/History/page.tsx
@@ -1,8 +1,9 @@
 import Copy from "@/components/Copy";
-import { Table, TableHeader, TableRow, TableHead, TableBody, TableCell, TableCaption } from "@/components/ui/table"
+import { Table, TableHeader, TableRow, TableHead, TableBody, TableCell, TableCaption } from "@/components/ui/table";
 import { db } from "@/lib/db";
-import { auth } from "@clerk/nextjs/server"
+import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
+
 export default async function History() {
   try {
     const { userId } = auth();
@@ -14,49 +15,32 @@ export default async function History() {
         userId: userId as string,
       },
     });
-    
-    return (
-      <div className="min-h-screen overflow-y-auto container mx-auto ">
-               <div className="min-h-screen overflow-y-auto container mx-auto">
-            <Table>
-                <TableCaption>A list of your recent invoices.</TableCaption>
-                <TableHeader>
-                    <TableRow>
-                        <TableHead className="w-[100px]">
-                            index
-                        </TableHead>
-                        <TableHead>
-                            Slug
-                        </TableHead>
-                        <TableHead>
-                            Output
-                        </TableHead>
-                        <TableHead className="text-right">
-                            Actions
-                        </TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    {userHistory.map((history:any , index:number) => (
-                        <TableRow key={history.id}>
-                            <TableCell className="font-medium">
-                                {index + 1}
-                            </TableCell>
-                            <TableCell>
-                                {history.slug}
-                            </TableCell>
-                            <TableCell className="truncate max-w-40">
-                                {history.output}
-                            </TableCell>
-                            <TableCell className="text-right">
-                                    <Copy text={history.output} />
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
 
-        </div>
+    return (
+      <div className="min-h-screen overflow-y-auto container mx-auto">
+        <Table>
+          <TableCaption>A list of your recent invoices.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Index</TableHead>
+              <TableHead>Slug</TableHead>
+              <TableHead>Output</TableHead>
+              <TableHead className="text-right">Actions</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {userHistory.map((history: any, index: number) => (
+              <TableRow key={history.id}>
+                <TableCell className="font-medium">{index + 1}</TableCell>
+                <TableCell>{history.slug}</TableCell>
+                <TableCell className="truncate max-w-40">{history.output}</TableCell>
+                <TableCell className="text-right">
+                  <Copy text={history.output} />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </div>
     );
   } catch (error) {
--- components/Copy.tsx
+++ components/Copy.tsx
@@ -1,20 +1,25 @@
-"use client"
-import React from 'react'
-import { Button } from './ui/button'
-import { CopyIcon } from 'lucide-react'
-import { toast } from './ui/use-toast'
-const Copy = ({output}:any) => {
-    const handleCopy = () => {
-        navigator.clipboard.writeText(output);
-        toast({
-            description: "Copied to clipboard 👏",
-          })
-    }
+'use client';
+
+import React from 'react';
+import { Button } from './ui/button';
+import { CopyIcon } from 'lucide-react';
+import { toast } from './ui/use-toast';
+
+const Copy = ({ text }: any) => {
+  console.log(text, "text");
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(text);
+    toast({
+      description: "Copied to clipboard 👏",
+    });
+  };
+
   return (
-    <Button variant="outline" size="icon" onClick={handleCopy} className='cursor-pointer'>
+    <Button variant="outline" size="icon" onClick={handleCopy} className="cursor-pointer">
       <CopyIcon className="h-4 w-4" />
     </Button>
-  )
-}
+  );
+};
 
-export default Copy
+export default Copy;
