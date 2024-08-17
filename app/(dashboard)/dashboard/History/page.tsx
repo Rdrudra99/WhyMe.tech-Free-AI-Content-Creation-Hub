55f7dee439abd174d809c0e9b8191370d5b606a1
Rudra Narayan Boitei <rd.rudra99@gmail.com>
2024-08-17 12:20:13 +0530

    commit

--- app/(dashboard)/dashboard/History/page.tsx
+++ app/(dashboard)/dashboard/History/page.tsx
@@ -1,9 +1,66 @@
-import React from 'react'
-const page = () => {
-  return (
-    <div className='flex justify-center items-center min-h-screen overflow-y-scroll'>
-        <h1>History page in development process</h1>
-    </div>
-  )
+import Copy from "@/components/Copy";
+import { Table, TableHeader, TableRow, TableHead, TableBody, TableCell, TableCaption } from "@/components/ui/table"
+import db from "@/lib/db";
+import { auth } from "@clerk/nextjs/server"
+import { NextResponse } from "next/server";
+export default async function History() {
+  try {
+    const { userId } = auth();
+    if (!userId) {
+      return NextResponse.redirect("/login");
+    }
+    const userHistory = await db.aiContent.findMany({
+      where: {
+        userId: userId as string,
+      },
+    });
+    
+    return (
+      <div className="min-h-screen overflow-y-auto container mx-auto ">
+               <div className="min-h-screen overflow-y-auto container mx-auto">
+            <Table>
+                <TableCaption>A list of your recent invoices.</TableCaption>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="w-[100px]">
+                            index
+                        </TableHead>
+                        <TableHead>
+                            Slug
+                        </TableHead>
+                        <TableHead>
+                            Output
+                        </TableHead>
+                        <TableHead className="text-right">
+                            Actions
+                        </TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {userHistory.map((history:any , index:number) => (
+                        <TableRow key={history.id}>
+                            <TableCell className="font-medium">
+                                {index + 1}
+                            </TableCell>
+                            <TableCell>
+                                {history.slug}
+                            </TableCell>
+                            <TableCell className="truncate max-w-40">
+                                {history.output}
+                            </TableCell>
+                            <TableCell className="text-right">
+                                    copy
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+
+        </div>
+      </div>
+    );
+  } catch (error) {
+    console.error("Error fetching user history:", error);
+    return NextResponse.json({ error: "Failed to fetch user history" }, { status: 500 });
+  }
 }
-export default page
--- lib/db.ts
+++ lib/db.ts
@@ -1,2 +1,6 @@
-import {PrismaClient} from "@prisma/client";
- export const db = new PrismaClient();
+// src/lib/prisma.js
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default prisma;
