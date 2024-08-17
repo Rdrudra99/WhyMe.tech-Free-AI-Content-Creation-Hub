6fa57f7f836e1c4eda2337a2dcf785fc399609f7
Rudra Narayan Boitei <rd.rudra99@gmail.com>
2024-08-17 12:36:59 +0530

    copy button add

--- app/(dashboard)/dashboard/History/page.tsx
+++ app/(dashboard)/dashboard/History/page.tsx
@@ -49,7 +49,7 @@ export default async function History() {
                                 {history.output}
                             </TableCell>
                             <TableCell className="text-right">
-                                    copy
+                                    <Copy text={history.output} />
                             </TableCell>
                         </TableRow>
                     ))}
